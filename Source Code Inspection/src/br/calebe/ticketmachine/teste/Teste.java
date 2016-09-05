3532a96668c77dcc4a40da4916f507169a98a55e
luucasportella <lucas_portellasantos@yahoo.com.br>
2016-09-05 09:02:04 -0300

    Classe Teste implementada.

--- Source Code Inspection/src/br/calebe/ticketmachine/teste/Teste.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/teste/Teste.java
@@ -5,6 +5,12 @@
  */
 package br.calebe.ticketmachine.teste;
 
+import br.calebe.ticketmachine.core.TicketMachine;
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Lucas
@@ -13,6 +19,17 @@ public class Teste {
    
     public static void main(String[] args) {
         
+        TicketMachine tm = new TicketMachine(10);
+        try {
+            tm.inserir(5);
+            tm.inserir(5);
+            tm.getSaldo();
+            
+            System.out.println(tm.imprimir());
+        } catch (PapelMoedaInvalidaException ex) {
+            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SaldoInsuficienteException ex) {
+            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
 }
